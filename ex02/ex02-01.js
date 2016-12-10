7a336d79af429f6e0bc3cea64db5129bc079a0f5
Michael Bl <michael414bl@gmail.com>
2016-12-10 20:38:17 +0700

    basically perform ex02

--- ex02/ex02-01.js
+++ ex02/ex02-01.js
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  */
 var questionBaseHandler = (function(questionArr) {
   // generate an array filled with a natural sequence [0,1,2,...] and shuffle it
@@ -8,7 +8,7 @@ var questionBaseHandler = (function(questionArr) {
   if (length == 0) getNext = () => null;
   else if (length == 1) getNext = () => questionArr[0];
   else {
-    let enumerationOrder = shuffleArray(Array.apply(null, {length: length}).map(Number.call, Number));
+    let enumerationOrder = shuffleArray([...Array(length)].map((v, i) => i));
     let pointer = 0;
     getNext = function() {
       if (pointer >= length) {
